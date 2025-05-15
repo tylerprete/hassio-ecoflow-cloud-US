5f813f23ab3d550e7fdaeadb175c2ccdee1e5bae
Evan S <139392174+snell-evan-itt@users.noreply.github.com>
2025-05-15 18:48:18 -0500

    Create wave3.py
    Signed-off-by: Evan S <139392174+snell-evan-itt@users.noreply.github.com>

---
+++ custom_components/ecoflow_cloud/devices/public/wave3.py
@@ -0,0 +1,16 @@
+from .data_bridge import to_plain
+from ..internal.wave3 import wav3 as Internalwave3
+from ...api import EcoflowApiClient
+from ...sensor import StatusSensorEntity
+
+
+class wave3(Internalwave3):
+
+    def _prepare_data(self, raw_data) -> dict[str, any]:
+        res = super()._prepare_data(raw_data)
+        res = to_plain(res)
+        return res
+
+    def _status_sensor(self, client: EcoflowApiClient) -> StatusSensorEntity:
+        return StatusSensorEntity(client, self)
+
