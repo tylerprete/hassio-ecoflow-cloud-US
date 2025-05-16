546bc9988c53f3016df6b3b0c575e18f33b434d3
Evan S <139392174+snell-evan-itt@users.noreply.github.com>
2025-05-15 19:51:10 -0500

    Update wave3.py
    Signed-off-by: Evan S <139392174+snell-evan-itt@users.noreply.github.com>

--- custom_components/ecoflow_cloud/devices/public/wave3.py
+++ custom_components/ecoflow_cloud/devices/public/wave3.py
@@ -4,7 +4,7 @@ from ...api import EcoflowApiClient
 from ...sensor import StatusSensorEntity
 
 
-class wave3(Internalwave3):
+class Wave3(InternalWave3):
 
     def _prepare_data(self, raw_data) -> dict[str, any]:
         res = super()._prepare_data(raw_data)
